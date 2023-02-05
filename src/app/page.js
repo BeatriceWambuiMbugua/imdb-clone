09452a57b806a2942492e66ec5f62730546619f5
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-02-05 21:11:39 +0300

    install tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return <h1 className="text-3xl font-bold underline">Home Page</h1>;
 }
