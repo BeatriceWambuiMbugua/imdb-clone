fd8b8d9dbc3865c0ae2fa33f4ab22d5174ba0d0d
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-02-09 17:01:47 +0300

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <img src="spinner.svg" alt="loading" />
+    </div>
+  );
+}
