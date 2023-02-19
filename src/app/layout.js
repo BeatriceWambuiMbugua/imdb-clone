ee84d3757b5c18349bad780136a1cec77485dc06
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-02-19 11:07:46 +0300

    add search ability

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,22 +2,20 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Provider from "@/app/Provider";
 import Navbar from "@/components/Navbar";
-
+import SearchBox from "@/components/SearchBox";
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          {/* Header */}
+	return (
+		<html lang="en">
+			<body>
+				<Provider>
+					<Header />
+					<Navbar />
+					<SearchBox />
 
-          {/* SearchBar */}
-          <Header />
-          <Navbar />
-
-          {children}
-        </Provider>
-      </body>
-    </html>
-  );
+					{children}
+				</Provider>
+			</body>
+		</html>
+	);
 }
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react"; 
+import { useRouter } from "next/navigation";
+
+export default function SearchBox() {
+	const [search, setSearch] = useState("");
+    const router = useRouter();
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!search) return;
+        router.push(`/search/${search}`);
+    }
+	return (
+		<form className="flex max-w-6xl justify-between mx-auto items-center px-5" onSubmit={handleSubmit}>
+			<input
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+				type="text"
+				placeholder="Search keywords ..."
+				className="w-full h-14 rounded-sm placeholder-gray-500 bg-transparent outline-none flex-1"
+			/>
+			<button type="submit" className="text-amber-500 disabled:text-gray-500" disabled={!search}>
+				{" "}
+				Search{" "}
+			</button>
+		</form>
+	);
+}
